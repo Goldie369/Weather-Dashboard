8383003d20fb16b5df2e834e6e5764a4f8e1d105
Zlatelina Rizova <zlatelinarizova@gmail.com>
2023-04-30 18:31:18 -0500

    adding a coordinats for current weater and now when you click search it's posted on the app

--- assets/script.js
+++ assets/script.js
@@ -29,3 +29,18 @@ $("#search-btn").on("click", function () {
                 <div>${(((res.main.temp) - 273.15) * (9 / 5) + 32).toFixed(2)}</div>
                 <div>${res.wind.speed}</div>
                 </div>`
+                $.ajax({
+                    url: 'http://api.openweathermap.org/data/2.5/forecast?lat=' + coords[0].lat + '&lon=' + coords[0].lon + "&apiKey=" + weatherApiKey
+                }).then(function (res) {
+                    console.log(res)
+                    console.log("date", res.list[0].dt_txt.split(" ")[0])
+                    for (leti = 0; i < 4; i++) {
+                        resultsContainer.append(`<div><div>${res.list[i].dt_txt.split(" ")[0]}</div><div>${res.list[i].main.temp
+
+                            }</div><div>${res.list[i].wind.speed}</div></div>`)
+                    }
+                    //resultsContainer.innerHTML = update
+                })
+            })
+    });
+})
